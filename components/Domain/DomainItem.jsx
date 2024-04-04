c820c95f2dd227ca0a084370a98c6c7900076d2f
HalimjonHalimov <88800233+HalimjonHalimov@users.noreply.github.com>
2024-04-04 15:46:17 +0500

    Commit 11

---
+++ components/Domain/DomainItem.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function DomainItem({
+  domain,
+  status,
+  click,
+  connect,
+  percent,
+  bot,
+  percentE,
+  metaMask,
+  statistic,
+}) {
+  return (
+    <div className="w-full h-auto grid grid-cols-9 gap-2 text-[#F5F5F5] text-xs font-normal">
+      <div>{domain}</div>
+      <div
+        className={`w-4 h-4 rounded-full ${
+          status == true ? "bg-[#64CB40]" : "bg-[#CB4040]"
+        } `}
+      ></div>
+      <div>{click}</div>
+      <div>{connect}</div>
+      <div>{percent}</div>
+      <div>{bot}</div>
+      <div>{percentE}</div>
+      <div
+        className={`w-4 h-4 rounded-full ${
+          metaMask == true ? "bg-[#64CB40]" : "bg-[#CB4040]"
+        } `}
+      ></div>
+      <div>{statistic}</div>
+    </div>
+  );
+}
--- pages/domains/index.jsx
+++ pages/domains/index.jsx
@@ -1,8 +1,288 @@
-import { Layout } from '@/components'
-import React from 'react'
+import { DomainItem, Layout, Paragraph, Title } from "@/components";
+import React from "react";
+import { LuPlusCircle } from "react-icons/lu";
 
 export default function Domains() {
   return (
-    <Layout>Domains</Layout>
-  )
+    <Layout>
+      <div className="w-full h-auto p-4 grid grid-cols-9 items-start gap-4">
+        <div className="col-span-8 w-full h-auto flex flex-col gap-4">
+          <div className="w-full h-auto bg-[#140E2D] py-3 px-5 rounded-xl flex justify-between items-center">
+            <div>
+              <Paragraph title={"Number of domains"} />
+              <Title>Login</Title>
+            </div>
+            <div>
+              <Paragraph title={"CT from METAMASK"} />
+              <Title>Login</Title>
+            </div>
+            <button
+              type="button"
+              className="flex justify-center rounded-full items-center gap-4 text-sm font-bold text-[#f5f5f5] bg-[#7E67FF] w-52 h-11 transition-all duration-200 ease-in-out hover:opacity-75"
+            >
+              <LuPlusCircle size={24} />
+              Add a domain
+            </button>
+          </div>
+          <div className="w-full h-auto bg-[#19113E] py-3 px-5 rounded-xl">
+            <Title>The working panel</Title>
+            <div className="w-full h-auto grid grid-cols-9 gap-2">
+              <span className="text-[#605D6E] text-xs font-normal">Domain</span>
+              <span className="text-[#605D6E] text-xs font-normal">Status</span>
+              <span className="text-[#605D6E] text-xs font-normal">Clicks</span>
+              <span className="text-[#605D6E] text-xs font-normal">
+                Connect
+              </span>
+              <span className="text-[#605D6E] text-xs font-normal">
+                Percent
+              </span>
+              <span className="text-[#605D6E] text-xs font-normal">Bots</span>
+              <span className="text-[#605D6E] text-xs font-normal">
+                Percent. En.
+              </span>
+              <span className="text-[#605D6E] text-xs font-normal">
+                CT MetaMask
+              </span>
+              <span className="text-[#605D6E] text-xs font-normal">
+                Statistics
+              </span>
+            </div>
+            <div className="w-full h-auto my-4 flex flex-col justify-start items-start gap-2">
+              <DomainItem
+                domain={"12/04/2024"}
+                status={true}
+                click={"250.30$"}
+                connect={"250.30$"}
+                percent={"250.30$"}
+                bot={"250.30$"}
+                percentE={"250.30$"}
+                metaMask={true}
+                statistic={"250.30$"}
+              />
+              <DomainItem
+                domain={"12/04/2024"}
+                status={true}
+                click={"250.30$"}
+                connect={"250.30$"}
+                percent={"250.30$"}
+                bot={"250.30$"}
+                percentE={"250.30$"}
+                metaMask={true}
+                statistic={"250.30$"}
+              />
+              <DomainItem
+                domain={"12/04/2024"}
+                status={true}
+                click={"250.30$"}
+                connect={"250.30$"}
+                percent={"250.30$"}
+                bot={"250.30$"}
+                percentE={"250.30$"}
+                metaMask={true}
+                statistic={"250.30$"}
+              />
+              <DomainItem
+                domain={"12/04/2024"}
+                status={true}
+                click={"250.30$"}
+                connect={"250.30$"}
+                percent={"250.30$"}
+                bot={"250.30$"}
+                percentE={"250.30$"}
+                metaMask={true}
+                statistic={"250.30$"}
+              />
+              <DomainItem
+                domain={"12/04/2024"}
+                status={true}
+                click={"250.30$"}
+                connect={"250.30$"}
+                percent={"250.30$"}
+                bot={"250.30$"}
+                percentE={"250.30$"}
+                metaMask={true}
+                statistic={"250.30$"}
+              />
+              <DomainItem
+                domain={"12/04/2024"}
+                status={true}
+                click={"250.30$"}
+                connect={"250.30$"}
+                percent={"250.30$"}
+                bot={"250.30$"}
+                percentE={"250.30$"}
+                metaMask={true}
+                statistic={"250.30$"}
+              />
+              <DomainItem
+                domain={"12/04/2024"}
+                status={true}
+                click={"250.30$"}
+                connect={"250.30$"}
+                percent={"250.30$"}
+                bot={"250.30$"}
+                percentE={"250.30$"}
+                metaMask={true}
+                statistic={"250.30$"}
+              />
+              <DomainItem
+                domain={"12/04/2024"}
+                status={true}
+                click={"250.30$"}
+                connect={"250.30$"}
+                percent={"250.30$"}
+                bot={"250.30$"}
+                percentE={"250.30$"}
+                metaMask={true}
+                statistic={"250.30$"}
+              />
+              <DomainItem
+                domain={"12/04/2024"}
+                status={true}
+                click={"250.30$"}
+                connect={"250.30$"}
+                percent={"250.30$"}
+                bot={"250.30$"}
+                percentE={"250.30$"}
+                metaMask={true}
+                statistic={"250.30$"}
+              />
+              <DomainItem
+                domain={"12/04/2024"}
+                status={true}
+                click={"250.30$"}
+                connect={"250.30$"}
+                percent={"250.30$"}
+                bot={"250.30$"}
+                percentE={"250.30$"}
+                metaMask={true}
+                statistic={"250.30$"}
+              />
+              <DomainItem
+                domain={"12/04/2024"}
+                status={true}
+                click={"250.30$"}
+                connect={"250.30$"}
+                percent={"250.30$"}
+                bot={"250.30$"}
+                percentE={"250.30$"}
+                metaMask={true}
+                statistic={"250.30$"}
+              />
+              <DomainItem
+                domain={"12/04/2024"}
+                status={true}
+                click={"250.30$"}
+                connect={"250.30$"}
+                percent={"250.30$"}
+                bot={"250.30$"}
+                percentE={"250.30$"}
+                metaMask={true}
+                statistic={"250.30$"}
+              />
+              <DomainItem
+                domain={"12/04/2024"}
+                status={false}
+                click={"250.30$"}
+                connect={"250.30$"}
+                percent={"250.30$"}
+                bot={"250.30$"}
+                percentE={"250.30$"}
+                metaMask={false}
+                statistic={"250.30$"}
+              />
+              <DomainItem
+                domain={"12/04/2024"}
+                status={false}
+                click={"250.30$"}
+                connect={"250.30$"}
+                percent={"250.30$"}
+                bot={"250.30$"}
+                percentE={"250.30$"}
+                metaMask={false}
+                statistic={"250.30$"}
+              />
+              <DomainItem
+                domain={"12/04/2024"}
+                status={false}
+                click={"250.30$"}
+                connect={"250.30$"}
+                percent={"250.30$"}
+                bot={"250.30$"}
+                percentE={"250.30$"}
+                metaMask={false}
+                statistic={"250.30$"}
+              />
+              <DomainItem
+                domain={"12/04/2024"}
+                status={false}
+                click={"250.30$"}
+                connect={"250.30$"}
+                percent={"250.30$"}
+                bot={"250.30$"}
+                percentE={"250.30$"}
+                metaMask={false}
+                statistic={"250.30$"}
+              />
+              <DomainItem
+                domain={"12/04/2024"}
+                status={false}
+                click={"250.30$"}
+                connect={"250.30$"}
+                percent={"250.30$"}
+                bot={"250.30$"}
+                percentE={"250.30$"}
+                metaMask={false}
+                statistic={"250.30$"}
+              />
+              <DomainItem
+                domain={"12/04/2024"}
+                status={false}
+                click={"250.30$"}
+                connect={"250.30$"}
+                percent={"250.30$"}
+                bot={"250.30$"}
+                percentE={"250.30$"}
+                metaMask={false}
+                statistic={"250.30$"}
+              />
+              <DomainItem
+                domain={"12/04/2024"}
+                status={false}
+                click={"250.30$"}
+                connect={"250.30$"}
+                percent={"250.30$"}
+                bot={"250.30$"}
+                percentE={"250.30$"}
+                metaMask={false}
+                statistic={"250.30$"}
+              />
+              <DomainItem
+                domain={"12/04/2024"}
+                status={false}
+                click={"250.30$"}
+                connect={"250.30$"}
+                percent={"250.30$"}
+                bot={"250.30$"}
+                percentE={"250.30$"}
+                metaMask={false}
+                statistic={"250.30$"}
+              />
+              <DomainItem
+                domain={"12/04/2024"}
+                status={false}
+                click={"250.30$"}
+                connect={"250.30$"}
+                percent={"250.30$"}
+                bot={"250.30$"}
+                percentE={"250.30$"}
+                metaMask={false}
+                statistic={"250.30$"}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
